1f307ed8dd68d7c1a2bed4f0e8dd5b18b86778de
evanhan6561 <54378519+evanhan6561@users.noreply.github.com>
2020-09-27 22:16:45 -0500

    Trying to deploy on aws, making changes

---
+++ backend/build/precache-manifest.0e697279f00276d124cad4dd8891573a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d77459f03416b81f60685517aa2420c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d4f243de109c0b23e9d",
+    "url": "/static/css/main.adbd3a98.chunk.css"
+  },
+  {
+    "revision": "82790ef13cc7f9a9e596",
+    "url": "/static/js/2.cb5b1d89.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.cb5b1d89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4f243de109c0b23e9d",
+    "url": "/static/js/main.64416186.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "d2e94b4c77f9e62b56e70fc5abceaab4",
+    "url": "/static/media/tomato.d2e94b4c.svg"
+  }
+]);
--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,7 @@ app.use(session({
 // Landing Page
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
+//   res.sendFile(path.join('..', 'frontend', 'build', 'index.html'));
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
